8875cdf32830eaeb7cee3aaf90f535d7c4f0d606
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-07-15 23:52:12 -0400

    Update 2) Setting Out to C++.cpp

--- 2) Setting Out to C++.cpp
+++ 2) Setting Out to C++.cpp
@@ -1,12 +1,19 @@
-// myfirst.cpp -- displays a message
 
-#include <iostream>
+//-------------------------------------------------------------Chapter 2: Setting Out to C++------------------------------------------------------------------------------
 
-int main()
-{
-    using namespace std;
-    cout << "Come up and C++ some time.";
-    cout << endl;
-    cout << "You won't regret it!" << endl;
-    return 0;
-}
+
+
+
+
+// This function displays a message
+
+#include <iostream>                         //A PREPROCESSOR directive
+
+int main()                                  //function header
+{                                           //Start of function body
+    using namespace std;                    //Make definitions visible
+    cout << "Come up and C++ some time.";   //Message
+    cout << endl;                           //Start a new line
+    cout << "You won't regret it!" << endl; //More output
+    return 0;                               //Terminate main()
+}                                           //End of function body
