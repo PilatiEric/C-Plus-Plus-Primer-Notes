d373f8015a8f7fc03862a59fd6f9c7eaff462068
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-07-22 20:36:54 -0400

    Update 2) Setting Out to C++.cpp

--- 2) Setting Out to C++.cpp
+++ 2) Setting Out to C++.cpp
@@ -13,8 +13,15 @@ int main()                                  //function header
 {                                           //Start of function body
     using namespace std;                    //Make definitions visible
     cout << "Come up and C++ some time.";   //Message
-                                        `   //Cout is the equivalent of Print, Console.Println, Console.Out.Prinln
+                                            //Cout is the equivalent of Print, Console.Println, Console.Out.Prinln
     cout << endl;                           //Start a new line
     cout << "You won't regret it!" << endl; //More output
     return 0;                               //Terminate main()
 }                                           //End of function body
+
+int testing()
+{
+    using namespace std;
+    cout << "This is just a test.";
+    return 0;
+}
