fe195283cb8d5b909f8a54bac8b45313e92e3f33
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-08-15 03:24:53 -0400

    Exercises Completed

--- 2) Chapter Review and Exercises.cpp
+++ 2) Chapter Review and Exercises.cpp
@@ -1,3 +1,23 @@
+//This main() is just to make sure there are no errors
+    #include <iostream>
+    using namespace std;
+
+    //Function Prototypes
+    int NameAddress();
+    int Furlongs();
+    int TheMice();
+    int TheyRun();
+
+
+//Main Method
+    int main()
+    {
+        NameAddress();
+        Furlongs();
+        TheMice();
+        TheyRun();
+        return 0;
+    }
 /*
 ---------------------------Chapter 2 Review----------------------------------
 
@@ -19,3 +39,39 @@
     b) Place "using std::cout" within the main()
     c) Use std::cout
 */
+
+//-----------------------Chapter 2 Exercises----------------------------
+
+//1)
+    int NameAddress()
+    {
+        cout << "My name is Princess Doughnut the Queen Anne Chonk" << endl;
+        cout << "I live at 1234 Some Place Street Street Avenue Boulavard of Broken Dreams Circle Way" << endl << endl;
+        return 0;
+    }
+
+//2)
+    int Furlongs()
+    {
+        int FurlongsDis;
+        cout << "Please provide a distance in furlongs: ";
+        cin >> FurlongsDis;
+        FurlongsDis /= 220;
+        cout << "That is a total of " << FurlongsDis << " yards." << endl << endl;
+        return 0;
+    }
+
+//3)
+    int TheMice()
+    {
+        cout << "Three blind mice" << endl;
+        cout << "Three blind mice" << endl;
+        return 0;
+    }
+
+    int TheyRun ()
+    {
+        cout << "See how they run" << endl;
+        cout << "See how they run" << endl << endl;
+        return 0;
+    }
