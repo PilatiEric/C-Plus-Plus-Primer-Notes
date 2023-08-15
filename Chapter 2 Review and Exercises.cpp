2294b5534e6b18cb314c3a9e5139a924f0fb795e
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-08-14 22:59:24 -0400

    New chapter review

---
+++ Chapter 2 Review and Exercises.cpp
@@ -0,0 +1,21 @@
+/*
+---------------------------Chapter 2 Review----------------------------------
+
+1) Functions
+2) Gives access to the iostream preprocessor
+3) Allows the use of the stf library
+4) cout << "Hello, World" << endl;
+5) int cheeses;
+6) cheeses = 32;
+7) cin >> cheeses;
+8) cout << "We have " << cheeses << " variety of cheese."
+9) a) The function takes in a double and returns an int
+   b) The function takes in an int and doesn't return anything
+   c) The function doesn't take in anything and returns an int
+10) The keyword "return" has to be used whenever a function is indicated to return anything but type void
+11) It means that access to the namespace std has not been granted
+    Ways to solve:
+    a) Place "using namespace std;" within the main()
+    b) Place "using std::cout" within the main()
+    c) Use std::cout
+*/
