e78aa4bdd523ef27f20ff884280e0dfaa256fd3a
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-09-05 20:45:35 -0400

    More notes

--- 3) Dealing with Data.cpp
+++ 3) Dealing with Data.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <climits>                          //Allows functions that show data type limits
+#include <climits>             //Allows functions that show data type limits
+#define ZERO 0                 //Makes "ZERO" the symbol for the 0 value     
 
 //Function Prototypes
 void TypeLimit();
+void UnsignedTypes();
 
 
 
@@ -12,6 +14,8 @@ void TypeLimit();
 int main()
 {
     TypeLimit();
+    UnsignedTypes();
+    return 0;
 }
 
 
@@ -30,5 +34,36 @@ void TypeLimit()
 
     cout << "Maximum values:" << endl;
     cout << "int: " << n_int << endl;
-    cout << "short: " << n_short << endl;
+    cout << "short: " << n_short << endl << endl;
+}
+
+void UnsignedTypes()
+{
+    //Unsigned version of int types start the limit of the numbers from 0
+    //This effectively doubles how high of a number an int type can hold
+    unsigned short change;
+    unsigned int rovert;
+    unsigned quarterback; //This is the same as "unsigned int quarterback"
+    unsigned long gone;
+    unsigned long long lang_lang;
+
+
+    short sam = SHRT_MAX;         //Initialize the variable to the max value
+    unsigned short sue = sam;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue <<
+            " dollars deposited." << endl <<
+            "Add $1 to each account." << endl << "Now ";
+    sam += 1;
+    sue += 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue <<
+            " dollars deposited. \nPoor Sam!" << endl;
+    sam = ZERO;
+    sue = ZERO;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue <<
+            " dollars deposited." << endl <<
+            "Take $1 from each account." << endl << "Now ";
+    sam -= 1;
+    sue -= 1;
+    cout << "Sam has " << sam << " dollars and Sue has " << sue <<
+            " dollars deposited." << endl << "Lucky Sue!" << endl << endl;
 }
